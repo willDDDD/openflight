49b221d8942bcc2765c47c31080bf690fa68bd61
zfeng13 <zfeng13@illinois.edu>
2020-11-22 12:12:08 +0800

    zfeng13 stupid commit, ignore it

--- project/main.cpp
+++ project/main.cpp
@@ -3,16 +3,5 @@
 #include "readFromFile.hpp"
 
 int main(int argc, const char * argv[]) {
-	std::cout << "Filename: " << "airport_sample.dat" << std::endl;
 	
-	// convert file to string representation
-	std::cout << "String Representation:" << std::endl;
-	std::cout << file_to_string("airport_sample.dat") << std::endl;
-	
-	// convert file to vector representation
-	std::cout << "Vector Representation:" << std::endl;
-	std::vector<std::string> vectorRepr = file_to_vector("airport_sample.dat");
-	for (auto word : vectorRepr) {
-		std::cout << word << std::endl;
-	}	
 }
