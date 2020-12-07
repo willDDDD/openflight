b4c7fad630f9c5d86a6bed6295133e03d95410da
zfeng13 <zfeng13@illinois.edu>
2020-12-07 23:09:18 +0800

    wirte some test cases for readFromFile and graph

--- project/tests/tests.cpp
+++ project/tests/tests.cpp
@@ -1,15 +1,61 @@
-// #include <string>
-// #include <vector>
+#include <string>
+#include <vector>
+#include "../catch/catch.hpp"
+#include "../readFromFile.cpp"
+#include "../graph.h"
+#include "../graphDrawing.h"
+#include "../vertex.h"
+#include "../edge.h"
+#include "../priorityQueue.h"
 
-// #include "../catch/catch.hpp"
-// #include "../readFromFile.hpp"
 
 
-// TEST_CASE("Verify that file_to_string works on a small example") {
-// 	std::string res = file_to_string("tests/smallSample.txt");
-// 	// always check "expected" == "actual" --> be consistent
-// 	REQUIRE("hello\nstudents\nof\ncs\n225\n!!!\n" == res);
-// }
+TEST_CASE("readFromFile: test read_airport on the sample") {
+	vector<Vertex> res = read_airport("airport_sample.dat");
+	// always check "expected" == "actual" --> be consistent
+	REQUIRE(1 == res[0].id);
+}
+TEST_CASE("readFromFile: test read_airline on the sample") {
+	vector<Edge> res = read_route("route_sample.dat");
+	// always check "expected" == "actual" --> be consistent
+	REQUIRE(16624 == res[0].id);
+}
+TEST_CASE("readFromFile: test split_vertex") {
+	ifstream wordsFile("airport_sample.dat");
+    string line;
+    getline(wordsFile, line);
+    string s = line;
+    REQUIRE(1 == split_vertex(s).id);
+}
+TEST_CASE("readFromFile: test split_route") {
+    ifstream wordsFile("route_sample.dat");
+    string line;
+    getline(wordsFile, line);
+    string s = line;
+    REQUIRE(16624 == split_edge(s).id);
+}
+
+
+vector<Vertex> vecOfV = read_airport("airport.dat");
+vector<Edge> vecOfE = read_route("route.dat");
+Graph g;
+TEST_CASE("test build") {
+    g.build(vecOfE, vecOfV);
+    REQUIRE("Goroka" == g.arrOfVertices[1].city);
+}
+TEST_CASE("test BFS") {
+
+}
+TEST_CASE("test getAllMinorityGroups") {
+
+}
+TEST_CASE("test getExactMinorityByNum") {
+
+}
+TEST_CASE("test getExactMinorityByV") {
+
+}
+
 
 // TEST_CASE("Verify that file_to_vector works on a small example") {
 // 	std::vector<std::string> res = file_to_vector("tests/smallSample.txt");
