2c9831046740cbd1e9ea3ac7f98f03fcfd53db22
Xinlong Dai <xinlong3@illinois.edu>
2020-12-06 18:03:39 -0600

    building a 1-indexed priority queue, added method to resize the array for heap

--- project/priorityQueue.cpp
+++ project/priorityQueue.cpp
@@ -1,1 +1,16 @@
 #include "priorityQueue.h"
+
+void init_id_to_dist() {
+    id_to_dist.resize(VECTOR_MAX_SIZE);
+}
+
+void resizeArray() {
+    unsigned newAfhSize = afhSize * 2;
+    int * newArr = new int[newAfhSize];
+    for (unsigned i = 0; i <= afhSize; i++) {
+        newArr[i] = arr_for_heap_[i];
+    }
+    delete [] arr_for_heap_;
+    arr_for_heap_ = newArr;
+    afhSize = newAfhSize;
+}
--- project/priorityQueue.h
+++ project/priorityQueue.h
@@ -1,1 +1,23 @@
 #pragma once
+#include <map>
+#define VECTOR_MAX_SIZE 14111
+
+class priorityQueue {
+    public:
+        /*
+         * id_to_dist_ map the airport id to the its distance to the source airport
+         * distances recorded here are subject to change
+         */
+        vector<int> id_to_dist_;
+        /*
+         * The array that represents the heap
+         */
+        int* arr_for_heap_;
+        /*
+         * the current size of arr_for_heap_
+         */
+        unsigned afhSize;
+    private:
+        void init_id_to_dist();
+        void resizeArray();
+};
